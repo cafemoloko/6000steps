6faa46f83794406577711cfef7e4fa46d50af9a1
cafemoloko <karolina.pawlaczyk@gmail.com>
2023-08-06 10:21:31 +0200

    photolist

---
+++ create_photolist.py
@@ -0,0 +1,20 @@
+# creates txt file to fetch all filenames (for photo gallery)
+
+import os
+
+directory_path = './photos/thumbnails'
+output_file = 'photoList.txt'
+
+try:
+    filenames = [filename for filename in os.listdir(directory_path) if filename.endswith('.jpg')]
+    file_content = '\n'.join(filenames)
+    output_file_path = os.path.join(directory_path, output_file)
+
+    with open(output_file_path, 'w') as f:
+        f.write(file_content)
+
+    print('File list saved to', output_file_path)
+
+except Exception as e:
+    print('Error:', e)
+
