1b7da9afc87be40886c81dac8e5decd8eb9fb0c3
cafemoloko <karolina.pawlaczyk@gmail.com>
2023-08-06 10:11:26 +0200

    creates thumbnails only from new files

--- modify_files.py
+++ modify_files.py
@@ -5,10 +5,19 @@
 # thumbnails are not square (250x250 px). Height is 250 px, width depends on the resized image width 
 # some photos might end up rotated 90 degrees clockwise
 # "to_resize" folder is temporary. The input directory for the files to be resized should be "renamed"
+ 
 
 from PIL import Image
 import os
 
+
+def get_last_number():
+    if os.path.exists('last_number.txt'):
+        with open('last_number.txt', 'r') as f:
+            last_number = int(f.read())
+        return last_number
+    else:
+        return 0
 
 def resize_images(input_dir, output_dir):
     if not os.path.exists(output_dir):
@@ -36,17 +45,21 @@ def create_thumbnails(input_dir, output_dir, thumbnail_size=(250, 250)):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    last_number = get_last_number()
+    incremented_number = last_number + 1
+
     file_list = os.listdir(input_dir)
     file_list.sort()
 
     for filename in file_list:
-        if filename.endswith('.jpg'):
+        if filename.endswith(f'{incremented_number}.jpg'):
             input_path = os.path.join(input_dir, filename)
             output_path = os.path.join(output_dir, filename)
 
             with Image.open(input_path) as img:
                 img.thumbnail(thumbnail_size)
                 img.save(output_path)
+                print(f'Thumbnail created for: {filename}')
 
 if __name__ == "__main__":
 
